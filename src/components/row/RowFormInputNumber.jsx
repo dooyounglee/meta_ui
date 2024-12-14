db2eac0086639d3ae8cfc3d8ea76ed372d1c0126
doo <gostbaducking1@nate.com>
2024-12-14 23:56:47 +0900

    feat: add rowform number,color

---
+++ src/components/row/RowFormInputNumber.jsx
@@ -0,0 +1,34 @@
+import { forwardRef } from 'react';
+
+function RowFormInputText({ type,
+        title, id, name, placeholder,
+        defaultValue, value, onChange,
+        list, // radio, checkbox, select
+        checkedValue, checked, // radio, checkbox
+        rows, cols, // textarea
+        mode, height, // editor
+        selected, keyStartDate, keyEndDate, startDate, endDate, handlerChange, // calender~
+        // selectCallback, onKeyDownCallback, reset, selectMe, // chooseUser
+        keyUsrNo, keyUsrNm, usrNo, usrNm, // findUser
+        readOnly, disabled, required,
+        onKeyDown
+    }, ref) {
+    return (
+        <dl>
+            <dt><label>{title}</label>{required && <span className="req">필수</span>}</dt>
+            <dd>
+                <input className="f_input2 w_full" type="number"
+                    name={name} placeholder={placeholder}
+                    defaultValue={defaultValue}
+                    ref={el => ref.current && (ref.current[name] = el)}
+                    readOnly={!!readOnly}
+                    disabled={!!disabled}
+                    required={!!required}
+                    autoComplete='off'
+                />
+            </dd>
+        </dl>
+    )
+}
+
+export default forwardRef(RowFormInputText);
