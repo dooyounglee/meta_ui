73ae96d9c5a5a8bd8592d9d8b071e73a5614f0ca
doo <gostbaducking1@nate.com>
2024-12-14 22:07:28 +0900

    feat: add rowform select

---
+++ src/components/row/RowFormSelect.jsx
@@ -0,0 +1,44 @@
+import { forwardRef } from 'react';
+
+function RowFormTextarea({ type,
+        title, id, name, placeholder,
+        defaultValue, value, onChange,
+        list, options, // radio, checkbox, select
+        checkedValue, checked, // radio, checkbox
+        rows, cols, // textarea
+        mode, height, // editor
+        selected, keyStartDate, keyEndDate, startDate, endDate, handlerChange, // calender~
+        // selectCallback, onKeyDownCallback, reset, selectMe, // chooseUser
+        keyUsrNo, keyUsrNm, usrNo, usrNm, // findUser
+        readOnly, disabled, required,
+        onKeyDown
+    }, ref) {
+    return (
+        <dl>
+            <dt><label>{title}</label>{required && <span className="req">필수</span>}</dt>
+            <dd>
+                <label className="f_select w_150" htmlFor="sel1">
+                    <select
+                        ref={el => {
+                            ref.current[name] = el;
+                        }}
+                        defaultValue={defaultValue}
+                        onChange={e => {
+                            ref.current[name].value = e.target.value; 
+                        }}
+                        readOnly={readOnly}
+                        disabled={disabled}
+                    >
+                        {options.map((option, i) => {
+                            return (
+                                <option key={i} value={option.value}>{option.label}</option>
+                            )
+                        })}
+                    </select>
+                </label>
+            </dd>
+        </dl>
+    )
+}
+
+export default forwardRef(RowFormTextarea);
